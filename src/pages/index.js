e7a768a61f46cc06ee20759b5a0bd1b6dfa81cf0
Patryk <100696206+pwierzbieniec@users.noreply.github.com>
2024-03-23 22:09:41 +0100

    fix: new landing page (#4)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,7 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import { HomepageHeader } from './HomepageHeader';
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/podstawowe-informacje">
-            Więcej informacji
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
