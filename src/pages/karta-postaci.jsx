6f6e70794ea6af4383afdd53f6b7b165369d0743
Patryk <100696206+pwierzbieniec@users.noreply.github.com>
2025-02-18 22:16:00 +0100

    feat: karta postaci (#30)
    * feat: character form
    
    * feat: files changes

---
+++ src/pages/karta-postaci.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import styles from "./kp.module.css";
+import CharacterFormContent from "../components/CharacterFormContent";
+import { discordAuth, discordMe } from "../components/const/urls";
+
+const CharacterForm = () => {
+  const [accessToken, setAccessToken] = useState(null);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("code");
+
+    if (token) {
+      setAccessToken(token);
+      fetch(discordMe, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((response) => response.json())
+        .then((data) => setUserData(data))
+        .catch((error) => console.error("Error:", error));
+    }
+  }, []);
+
+  const redirectToDiscordAuth = () => {
+    window.location.href = discordAuth;
+  };
+
+  return (
+    <div className={(styles.container, styles.body)}>
+      {!accessToken ? (
+        <button className={styles.loginButton} onClick={redirectToDiscordAuth}>
+          Login with Discord
+        </button>
+      ) : (
+        <CharacterFormContent discordUserName={userData?.username} />
+      )}
+    </div>
+  );
+};
+
+export default CharacterForm;
