66ce34c2c5aed7e7b102c8a7b3dd9acbdb4f3fe1
pwierzbieniec <patryk.wierzbieniec@gmail.com>
2025-02-18 21:14:20 +0100

    feat: character form

---
+++ src/pages/const.js
@@ -0,0 +1,5 @@
+export const apiUrl =
+  "https://kn-form.azurewebsites.net/api/ProcessCharacterForm";
+export const discordAuth =
+  "https://discord.com/oauth2/authorize?client_id=712607067420229685&response_type=code&redirect_uri=https%3A%2F%2Fkn-discord.azurewebsites.net%2Fapi%2Fcallback&scope=identify";
+export const discordMe = "https://discord.com/api/users/@me";
